1ad72c163ef657a52187caa0ae33758754eb3134
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2020-12-04 09:37:24 +0530

    Updated with efficient way. Way better than before :D

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,93 +1,22 @@
+
+def slopeCount(l, addX, addY):
+    count = 0
+    posX = 0
+    posY = 0
+    while(posY < 322):
+        posX += addX
+        posX = posX % len(l[0])
+        posY += addY
+        if l[posY][posX] == '#':
+            count +=1 
+    return count
+
+
 def main():
     file = open('input.txt', 'r')
     s = file.read()
     l = s.split('\n')
-    posX = 0
-    posY = 0
-    slope2Count = 0
-    while True:
-        if posX == 30:
-            posX = -1
-        elif posX == 29:
-            posX = -2
-        elif posX == 28:
-            posX = -3
-        posX += 3
-        posY += 1
-        if l[posY][posX] == '#':
-            slope2Count += 1
-        if posY == 322:
-            break
-    print(f'Problem 1 answer: {slope2Count}')
-    slope1Count = 0
-    slope3Count = 0
-    slope4Count = 0
-    slope5Count = 0
-    posX = 0
-    posY = 0
-    while True:    
-        if posX == 30:
-            posX = -1
-        posX += 1
-        posY += 1
-        if l[posY][posX] == '#':
-            slope1Count += 1
-        if posY == 322:
-            break
-    posX = 0
-    posY = 0
-    while True:    
-        if posX == 30:
-            posX = -1
-        elif posX == 29:
-            posX = -2
-        elif posX == 28:
-            posX = -3
-        elif posX == 27:
-            posX = -4
-        elif posX == 26:
-            posX = -5
-        posX += 5
-        posY += 1
-        if l[posY][posX] == '#':
-            slope3Count += 1
-        if posY == 322:
-            break
-    posX = 0
-    posY = 0
-    while True:    
-        if posX == 30:
-            posX = -1
-        elif posX == 29:
-            posX = -2
-        elif posX == 28:
-            posX = -3
-        elif posX == 27:
-            posX = -4
-        elif posX == 26:
-            posX = -5
-        elif posX == 25:
-            posX = -6
-        elif posX == 24:
-            posX = -7
-        posX += 7
-        posY += 1
-        if l[posY][posX] == '#':
-            slope4Count += 1
-        if posY == 322:
-            break
-    posX = 0
-    posY = 0
-    while True:    
-        if posX == 30:
-            posX = -1
-        posX += 1
-        posY += 2
-        if l[posY][posX] == '#':
-            slope5Count += 1
-        if posY == 322:
-            break
-    print(f'Problem 2 Answer: {slope1Count * slope2Count * slope3Count * slope4Count * slope5Count}')
-
+    print(f'Answer Of First Problem: {slopeCount(l, 3, 1)}')
+    print(f'Answer of Second Problem: {slopeCount(l, 1, 1) * slopeCount(l, 3, 1) * slopeCount(l, 5, 1) * slopeCount(l, 7, 1) * slopeCount(l, 1, 2)}')
 
 main()
